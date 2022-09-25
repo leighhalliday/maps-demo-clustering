ebd01637f32ce4ff8748482fe26c9b9fc2332564
Leigh Halliday <leighhalliday@gmail.com>
2022-09-25 14:08:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Google Maps in React
+
+Data source: https://open.toronto.ca/dataset/street-tree-data/
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { useState, useCallback } from "react";
+import { GoogleMapProvider } from "@ubilabs/google-maps-react-hooks";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import SuperClusterAlgorithm from "../utils/superClusterAlgorithm";
+import trees from "../data/trees";
+
+export default function Index() {
+  <div>Map</div>;
+}
